0cacd746d2690c1f578f14b2dae05bc8d0a2fd3d
novatriansyah <novaajis20@gmail.com>
2026-06-02 22:20:27 +0700

    chore: migrate deployment to GitHub Actions and clean up old config

--- dump_compiled.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-use Illuminate\Support\Facades\Blade;
-
-echo Blade::compileString(file_get_contents('resources/views/frontend/components/layout.blade.php'));
